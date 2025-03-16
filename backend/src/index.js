96921fba9855ee334e7acd73a90f536a662a3331
amirhamza24 <ridoykhan7897890@gmail.com>
2025-03-16 16:08:37 +0600

    login, logout, profile update and checking api working

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 // const express = require("express");
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,6 +14,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
