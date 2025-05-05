4a24eb3991e68517fdce81e58da105c4d934264a
amirhamza24 <ridoykhan7897890@gmail.com>
2025-05-05 17:53:21 +0600

    home page updating

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -23,3 +23,30 @@ const AuthImagePattern = ({ title, subtitle }) => {
 };
 
 export default AuthImagePattern;
+
+// for theme changing bg
+// const AuthImagePattern = ({ title, subtitle }) => {
+//   return (
+//     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+//       <div className="max-w-md text-center mt-10">
+//         <div className="flex items-center justify-center">
+//           <div className="grid grid-cols-3 w-72 h-72 gap-3 mb-8">
+//             {[...Array(9)].map((_, i) => (
+//               <div
+//                 key={i}
+//                 className={`aspect-square rounded-2xl bg-blue-500/10 ${
+//                   i % 2 === 0 ? "animate-pulse" : ""
+//                 }`}
+//               />
+//             ))}
+//           </div>
+//         </div>
+
+//         <h2 className="text-2xl font-bold mb-4">{title}</h2>
+//         <p className="text-base-content/60">{subtitle}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AuthImagePattern;
