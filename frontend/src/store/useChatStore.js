14ca279cbbbcbe194d1a97ca142e2f1bb19934c2
amirhamza24 <ridoykhan7897890@gmail.com>
2025-05-21 18:11:24 +0600

    chatting message store 2.45.30

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -6,18 +6,30 @@ export const useChatStore = create((set) => ({
   messages: [],
   users: [],
   selectedUser: null,
-  isUserLoading: false,
-  isMessageLoading: false,
+  isUsersLoading: false,
+  isMessagesLoading: false,
 
   getUsers: async () => {
-    set({ isUserLoading: true });
+    set({ isUsersLoading: true });
     try {
       const res = await axiosInstance.get("/messages/users");
       set({ users: res.data });
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
-      set({ isUserLoading: false });
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
     }
   },
 }));
